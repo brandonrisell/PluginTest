cc6ab7a09a775c48b38beaed3585bc2bea06ad13
Brandon Risell <brisell@frontporch.com>
2017-02-27 13:24:29 -0800

    Add Share Plugin and update CustomTabs package to reproduce linker issue.

---
+++ PluginTest/PluginTestPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Plugin.Share;
+using Plugin.Share.Abstractions;
+using Xamarin.Forms;
+
+namespace PluginTest
+{
+    public partial class PluginTestPage : ContentPage
+    {
+        public PluginTestPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            var sharePlugin = CrossShare.Current;
+            var message = new ShareMessage() { Text = "Share Plugin!", Title = "Testing" };
+            sharePlugin.Share(message);
+        }
+    }
+}
